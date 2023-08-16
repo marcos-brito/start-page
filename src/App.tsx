a9626bfa85ad32524514a2dc1be6b57812f0ef26
Marcos Brito <marcos.brito0507@gmail.com>
2023-08-16 10:30:00 -0300

    feat: add a router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import "./App.css";
+import "./styles/App.css";
+import Home from "./pages/home/Home.tsx";
+import Settings from "./pages/settings/Settings.tsx";
+import NoMatch from "./pages/noMatch/NoMatch.tsx";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>Hello there</h1>
-    </>
-  );
+    return (
+        <>
+            {
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Home />}></Route>
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="*" element={<NoMatch />} />
+                    </Route>
+                </Routes>
+            }
+        </>
+    );
 }
 
 export default App;
